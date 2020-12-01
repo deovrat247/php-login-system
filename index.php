3e26d364f658a8664c578870d5baad352a90e3e5
deovrat <deovrat247@gmail.com>
2020-12-01 13:38:00 +0530

    added login form

--- index.php
+++ index.php
@@ -1,4 +1,90 @@
-<?php
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title>PHP Login System</title>
 
-echo "hello world";
- ?>
+     <!-- UIkit CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.5.10/dist/css/uikit.min.css" />
+
+
+   </head>
+   <body>
+
+<div class="uk-section uk-container ">
+
+  <div class="uk-child-width-expand@s " uk-grid>
+      <div>
+        <form class="uk-form-stacked">
+
+            <div class="uk-margin">
+                <label class="uk-form-label" for="form-stacked-text">Type Your Email</label>
+                <div class="uk-form-controls">
+                    <input class="uk-input" id="form-stacked-text" type="email" placeholder="Some text...">
+                </div>
+            </div>
+            <div class="uk-margin">
+                <label class="uk-form-label" for="form-stacked-text">Enter Password</label>
+                <div class="uk-form-controls">
+                    <input class="uk-input" id="form-stacked-text" type="password" placeholder="Some text...">
+                </div>
+            </div>
+            <div class="uk-margin">
+              <button class="uk-button uk-button-default" type="submit">Login</button>
+
+            </div>
+
+            <!-- <div class="uk-margin">
+                <label class="uk-form-label" for="form-stacked-select">Select</label>
+                <div class="uk-form-controls">
+                    <select class="uk-select" id="form-stacked-select">
+                        <option>Option 01</option>
+                        <option>Option 02</option>
+                    </select>
+                </div>
+            </div> -->
+      <!--
+            <div class="uk-margin">
+                <div class="uk-form-label">Radio</div>
+                <div class="uk-form-controls">
+                    <label><input class="uk-radio" type="radio" name="radio1"> Option 01</label><br>
+                    <label><input class="uk-radio" type="radio" name="radio1"> Option 02</label>
+                </div>
+            </div> -->
+
+        </form>
+      </div>
+      <div>
+          <!-- <div class="uk-card uk-card-default uk-card-body">Item</div> -->
+      </div>
+      <div>
+          <!-- <div class="uk-card uk-card-default uk-card-body">Item</div> -->
+      </div>
+  </div>
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     <!-- UIkit JS -->
+     <script src="https://cdn.jsdelivr.net/npm/uikit@3.5.10/dist/js/uikit.min.js"></script>
+     <script src="https://cdn.jsdelivr.net/npm/uikit@3.5.10/dist/js/uikit-icons.min.js"></script>
+   </body>
+ </html>
